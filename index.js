8b639f471fc67868e52e7fc5f01ce86c3381513a
PoohInwza007 <pattarapol.petcharat@gmail.com>
2025-09-25 22:57:23 +0700

    test

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.get("/", (req, res) => {
     res.render('home');
 });
 
+
+// path ของเมนูทุกอย่าง
 app.get('/menu', function (req, res) {
     const query = 'SELECT * FROM MenuItem;';
     db.all(query, (err, rows) => {
@@ -36,6 +38,32 @@ app.get('/menu', function (req, res) {
         }
         console.log(rows);
         res.render('menu', { data: rows });
+    });
+});
+
+// path ของเมนูหมวดหมู่ PASTA & RICE
+app.get('/pasta', function (req, res) {
+    const query = `SELECT * FROM MenuItem
+                   WHERE category_id = 2;`;
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+        }
+        console.log(rows);
+        res.render('menu_pasta', { data: rows });
+    });
+});
+
+// path ของเมนูหมวดหมู่ Steak
+app.get('/steak', function (req, res) {
+    const query = `SELECT * FROM MenuItem
+                   WHERE category_id = 3;`;
+    db.all(query, (err, rows) => {
+        if (err) {
+            console.log(err.message);
+        }
+        console.log(rows);
+        res.render('menu_steak', { data: rows });
     });
 });
 
---
+++ public/img/menu/menu.md
@@ -0,0 +1,10 @@
+## ชื่อโฟลเดอร์เก็บรูปเรียงตาม Catagory_id
+1	COMBO SET
+2	PASTA & RICE
+3	STEAK
+4	BURGER
+5	APPETIZER
+6	POP SODA
+
+
+
